d934cb142657f067d008379e220913e573f6d7ff
Rodrigo Alves <rodrigovieira1994@gmail.com>
2015-02-26 22:19:47 -0300

    Initial Commit

---
+++ Microtext/Microtext.h
@@ -0,0 +1,19 @@
+//
+//  Microtext.h
+//  Microtext
+//
+//  Created by Rodrigo Alves on 2/26/15.
+//  Copyright (c) 2015 Sgt. Peppers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Microtext.
+FOUNDATION_EXPORT double MicrotextVersionNumber;
+
+//! Project version string for Microtext.
+FOUNDATION_EXPORT const unsigned char MicrotextVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Microtext/PublicHeader.h>
+
+
